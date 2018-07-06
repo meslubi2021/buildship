37b38f671341d8334013c7dd3053f9ed11172224
Donát Csikós <csdonat@gmail.com>
2018-07-06 11:53:17 +0200

    Cleanup root project

--- .teamcity/Buildship/Project.kt
+++ .teamcity/Buildship/Project.kt
@@ -4,7 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2018_1.ParameterDisplay
 import jetbrains.buildServer.configs.kotlin.v2018_1.Project
 
 object Project : Project({
-    description = "Eclipse plugins for Gradle    http://eclipse.org/buildship"
+    description = "Eclipse plugins for Gradle http://eclipse.org/buildship"
 
     vcsRoot(GitHubVcsRoot)
 
@@ -14,26 +14,6 @@ object Project : Project({
         password("eclipse.downloadServer.password", "credentialsJSON:3b651471-b495-4e6e-9708-f2d7860c90f7", label = "Password", display = ParameterDisplay.HIDDEN)
         param("env.JAVA_TOOL_OPTIONS", "-Dfile.encoding=UTF8")
         password("eclipse.downloadServer.username", "credentialsJSON:23f3947f-45b2-46b8-83f6-9341c9b914f6", label = "Username", display = ParameterDisplay.HIDDEN)
-    }
-
-    features {
-        feature {
-            id = "PROJECT_EXT_67"
-            type = "project-graphs"
-            param("series", """
-                [
-                  {
-                    "type": "valueType",
-                    "title": "Build Duration (excluding Checkout Time)",
-                    "sourceBuildTypeId": "Tooling_Buildship_Commit_Eclipse42Build",
-                    "key": "BuildDurationNetTime"
-                  }
-                ]
-            """.trimIndent())
-            param("format", "text")
-            param("title", "Commit build time")
-            param("seriesTitle", "Serie")
-        }
     }
 
     cleanup {
