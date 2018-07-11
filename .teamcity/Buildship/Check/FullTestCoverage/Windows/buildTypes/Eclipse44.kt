dd7c2c373188e2507f435f9d8b9bca7c59378cb4
Donát Csikós <csdonat@gmail.com>
2018-07-11 15:47:02 +0200

    Clean up redundant retryBuild declaration

--- .teamcity/Buildship/Check/FullTestCoverage/Windows/buildTypes/Eclipse44.kt
+++ .teamcity/Buildship/Check/FullTestCoverage/Windows/buildTypes/Eclipse44.kt
@@ -2,7 +2,6 @@ package Buildship.Check.FullTestCoverage.Windows.buildTypes
 
 import Buildship.EclipseBuildTemplate
 import jetbrains.buildServer.configs.kotlin.v2018_1.BuildType
-import jetbrains.buildServer.configs.kotlin.v2018_1.triggers.retryBuild
 
 object Eclipse44 : BuildType({
     id("Full_Test_Coverage_Windows_Eclipse42_Java7")
@@ -15,13 +14,6 @@ object Eclipse44 : BuildType({
         param("compiler.location", """%windows.java7.oracle.64bit%\bin\javac""")
         param("eclipse.test.java.home", "%windows.java7.oracle.64bit%")
         param("env.JAVA_HOME", "%windows.java7.oracle.64bit%")
-    }
-
-    triggers {
-        retryBuild {
-            id = "retryBuildTrigger"
-            attempts = 2
-        }
     }
 
     requirements {
