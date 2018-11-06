d276487c42fc3ee37f0e3d819e7ae3c0788c0ac4
Donát Csikós <csdonat@gmail.com>
2018-11-06 14:57:21 +0100

    Merge patch to project settings

--- .teamcity/Buildship/Project.kt
+++ .teamcity/Buildship/Project.kt
@@ -14,7 +14,7 @@ object Project : Project({
 
     params {
         param("env.JAVA_TOOL_OPTIONS", "-Dfile.encoding=UTF8")
-        password("eclipse.downloadServer.password", "credentialsJSON:3b651471-b495-4e6e-9708-f2d7860c90f7", label = "Password", display = ParameterDisplay.HIDDEN)
+        password("eclipse.downloadServer.password", "credentialsJSON:e600b1ef-46bb-4ced-b45b-795da6964956", label = "Password", display = ParameterDisplay.HIDDEN)
         password("eclipse.downloadServer.username", "credentialsJSON:23f3947f-45b2-46b8-83f6-9341c9b914f6", label = "Username", display = ParameterDisplay.HIDDEN)
     }
 
