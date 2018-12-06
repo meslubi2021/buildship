733ed6cefa7cc9d2151d14c9efa6fb06d851d460
Donát Csikós <csdonat@gmail.com>
2018-12-06 17:54:44 +0100

    Merge generated patch

--- .teamcity/Buildship/GitHubVcsRoot.kt
+++ .teamcity/Buildship/GitHubVcsRoot.kt
@@ -10,8 +10,5 @@ object GitHubVcsRoot : GitVcsRoot({
     agentGitPath = "%env.TEAMCITY_GIT_PATH%"
     agentCleanFilesPolicy = GitVcsRoot.AgentCleanFilesPolicy.NON_IGNORED_ONLY
     useMirrors = false
-    authMethod = password {
-        userName = "gradlewaregitbot"
-        password = "credentialsJSON:4da2f871-8c1f-40d7-a147-860ebfae275e"
-    }
+    authMethod = anonymous()
 })
