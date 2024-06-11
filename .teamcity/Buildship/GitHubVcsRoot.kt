f112a2030508e835e543b1d99650bf0eb4266dc1
Donát Csikós <donat@gradle.com>
2024-06-11 14:54:48 +0200

    Define and branch spec

--- .teamcity/Buildship/GitHubVcsRoot.kt
+++ .teamcity/Buildship/GitHubVcsRoot.kt
@@ -5,6 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
 object GitHubVcsRoot : GitVcsRoot({
     name = "Buildship"
     url = "https://github.com/eclipse/buildship.git"
+    branch = "master"
+    branchSpec = "+:*"
     agentGitPath = "%env.TEAMCITY_GIT_PATH%"
     agentCleanFilesPolicy = AgentCleanFilesPolicy.NON_IGNORED_ONLY
     checkoutPolicy = AgentCheckoutPolicy.AUTO
