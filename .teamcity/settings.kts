16b210ea175dd277f2d24ddf442af77069cf54cf
Donát Csikós <donat@gradle.com>
2024-12-27 11:00:15 +0100

    Update dsl

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -29,5 +29,5 @@ node (Plugins -> teamcity-configs -> teamcity-configs:generate),
 the 'Debug' option is available in the context menu for the task.
 */
 
-version = "2023.11"
+version = "2024.12"
 project(Buildship.Project)
